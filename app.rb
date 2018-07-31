06f0fcb5f8fbb5c129ab0970640220a9b69ddb55
SimplyLena <simplylena02@yahoo.com>
2018-07-31 19:35:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Welcome to my life!"
+  end
+
+get '/name' do
+  "My name is LENA!"
+    end
+    
+    get '/hometown' do
+      "My hometown is Greensboro"
+    end
+    
+    get '/favorite-song' do
+      "My favorite song is This is America"
+    end
+
 end
